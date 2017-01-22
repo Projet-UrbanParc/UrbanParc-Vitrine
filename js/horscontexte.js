285bf2e7e751a76874d506f7832ab47b7b5fab48
TUIL Alexandre <tuil_alexandre@hotmail.com>
2017-01-22 16:43:37 +0100

    Exemple sur link2 (accrobranche) pour la fluiditée du switch background.
    Rajout du système box active sur hearder

--- js/horscontexte.js
+++ js/horscontexte.js
@@ -20,6 +20,7 @@ document.getElementById('link1').onclick = function() {
 //================================LINK 2 ================================/
 
 document.getElementById('link2').onmousedown = function() {
+    document.getElementById('header').classList.remove('box-active2');
     document.getElementById('section1').classList.remove('box-active');
     document.getElementById('section2').classList.remove('box-active2');
     document.getElementById('section3').classList.remove('box-active');
@@ -30,6 +31,7 @@ document.getElementById('link2').onclick = function() {
     document.getElementById('section1').classList.add('box-active');
     document.getElementById('section3').classList.add('box-active');
     document.getElementById('section2').classList.add('box-active2');
+    document.getElementById('header').classList.add('box-active2');
     return false;
 };
 //================================LINK 3 ==============================/
